b1910df6ba41dccc6ead3585a6ea5a44a76754ce
Saidur289 <saidur.riaz1@gmailcomm>
2025-02-22 13:29:22 +0600

    create provider for supply data all over project

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { DecrementCounter, incrementCounter, ResetCounter } from "../services/actions/CountrActions";
 
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    const count = useSelector((state) =>state.count)
+    const dispatch = useDispatch()
+    
     const handleIncrement = () => {
-        setCount((count) => count + 1)
+     dispatch(incrementCounter())
+    }
+    const handleDecrement = () => {
+     dispatch(DecrementCounter())
+    }
+    const handleReset = () => {
+     dispatch(ResetCounter())
     }
     return (
         <div>
                <h2>Counter App</h2>
             <h3>Count : {count}</h3>
-            <button className="py-2 px-5 bg-purple-800 text-white" onClick={handleIncrement}>Increment</button>
+            <button  className="py-2 px-5 bg-purple-800 text-white" onClick={handleIncrement}>Increment</button>
+            <button  className="py-2 px-5 bg-blue-800 text-white" onClick={handleDecrement}>Decrement</button>
+            <button  className="py-2 px-5 bg-green-800 text-white" onClick={handleReset}>Reset</button>
         </div>
     );
 };
